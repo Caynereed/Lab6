fb4ee59afae555897a0649a647a5f506b983378a
Caynereed <reed146@tritons.iowacentral.edu>
2023-11-13 09:58:59 -0600

    Initial Commit

---
+++ Lab6/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab6
+//
+//  Created by Cayne Reed on 11/13/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
